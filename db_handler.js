996a2b0f538b755856f7396a004b54876c4d3e50
lel <test@gmail.com>
2020-09-09 12:16:07 +0300

    addProductImage

--- db_handler.js
+++ db_handler.js
@@ -1,15 +1,24 @@
 import * as SQLite from 'expo-sqlite';
 import config from './config'
 
-export const createDBTables = () => {
-     const db = SQLite.openDatabase(config.getCell("DatabaseName"), config.getCell("DatabaseVersion"));
+const db = SQLite.openDatabase(config.getCell("DatabaseName"), config.getCell("DatabaseVersion"));
 
+const executeSql = (sql, args) => {
     db.transaction( (tr) => {
-        tr.executeSql(`CREATE TABLE IF NOT EXIST CategoryList(
-            productCategoryId INTEGER,
-            imageLink TEXT,
-            imageData TEXT)`);
+        tr.executeSql(sql, args);
     });
-
 };
 
+export const createDBTables = () => {
+    executeSql(`CREATE TABLE IF NOT EXIST CategoryList(
+        productCategoryId INTEGER,
+        imageLink TEXT,
+        imageData TEXT)`);
+};
+
+export const addProductImage = (productCategoryId, imageLink, imageData) => {
+    executeSql(`INSERT INTO CategoryList(
+        productCategoryId,
+        imageLink,
+        imageData) VALUES(?, ?, ?)`, [productCategoryId, imageLink, imageData]);
+};
