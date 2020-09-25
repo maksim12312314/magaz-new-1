06de7a10bd767745d18a6cc443b07408945ffa1a
pBinarySoul <pBinarySoul@gmail.com>
2020-09-25 21:45:53 +0300

    ItemControlButton -> OurIconButton

--- components/pages/Cart/CartItem/ItemCount/index.js
+++ components/pages/Cart/CartItem/ItemCount/index.js
@@ -4,7 +4,8 @@ import { stateContext, dispatchContext } from "../../../../../contexts";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faPlusCircle, faMinusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import styles from "./styles";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
+import OurIconButton from "../../../../OurIconButton";
 
 import {
     Plus,
@@ -13,58 +14,47 @@ import {
     ComputeTotalPrice,
 } from "../../../../../actions";
 
-const ItemControlButton = (props) =>
-{
-    const { onPress, icon } = props;
-    return (
-        <TouchableOpacity onPress={onPress} style={styles.button}>
-            <FontAwesomeIcon size={
-                Math.max(Dimensions.get("window").width, Dimensions.get("window").height) * .05
-                } color={"#fff"} icon={icon}/>
-        </TouchableOpacity>
-    )
-};
 
 /** Компонент, который отображает количество товаров в корзине */
 const ItemCount = (props) =>
 {
-    const state = useContext(stateContext);
     const dispatch = useContext(dispatchContext);
     const {id} = props;
     const { t } = useTranslation();
 
+    const plusPressed = (e) => {
+        // Добавляем 1 товар
+        dispatch(Plus(id));
+        dispatch(ComputeTotalPrice());
+    };
+    const minusPressed = (e) => {
+        // Вычитаем 1 товар
+        dispatch(Minus(id, dispatch, t));
+        dispatch(ComputeTotalPrice());
+    };
+    const deletePressed = (e) => {
+        Alert.alert(t("cartDeleteTitle"), t("cartDeleteMessage"), [
+                {
+                    text: t("cancel"),
+                    style: "cancel"
+                },
+                {
+                    text: t("ok"),
+                    onPress: () => {
+                        dispatch(DeleteFromCart(id, true));
+                        dispatch(ComputeTotalPrice());
+                    },
+                },
+            ],
+            {cancelable: false});
+    };
+
     return (
         <View style={styles.container}>
             <View style={styles.itemControl}>
-                <ItemControlButton icon={faPlusCircle} onPress={(e) =>
-                {
-                    // Добавляем 1 товар 
-                    dispatch(Plus(id));
-                    dispatch(ComputeTotalPrice());
-                }}/>
-                <ItemControlButton icon={faMinusCircle} onPress={(e) =>
-                {
-                    // Вычитаем 1 товар
-                    dispatch(Minus(id, dispatch, t));
-                    dispatch(ComputeTotalPrice());
-                }}/>
-                <ItemControlButton icon={faTimesCircle} onPress={(e) =>
-                {
-                    Alert.alert(t("cartDeleteTitle"), t("cartDeleteMessage"), [
-                        {
-                            text: t("cancel"),
-                            style: "cancel"
-                        },
-                        {
-                            text: t("ok"),
-                            onPress: () => {
-                                dispatch(DeleteFromCart(id, true));
-                                dispatch(ComputeTotalPrice());
-                            },
-                        },,
-                    ],
-                    {cancelable: false});
-                }}/>
+                <OurIconButton style={{margin: 1}} icon={faPlusCircle} onPress={plusPressed}/>
+                <OurIconButton style={{margin: 1}} icon={faMinusCircle} onPress={minusPressed}/>
+                <OurIconButton style={{margin: 1}} icon={faTimesCircle} onPress={deletePressed}/>
             </View>
         </View>
     );
