4d8583187c960755b70bbeace4b1a830180a25bd
pBinarySoul <pBinarySoul@gmail.com>
2020-10-09 11:25:35 +0300

    Different file for animations

---
+++ Animations.js
@@ -0,0 +1,50 @@
+import { Animated } from "react-native";
+
+export const ListAnimation = (x, y, totalHeight, itemHeight, itemWidth, index) => {
+    const position = Animated.subtract(index * itemHeight, y);
+    const isDisappearing = -itemHeight;
+    const isTop = 0;
+    const isBottom = totalHeight - itemHeight;
+    const isAppearing = totalHeight;
+    const translateX = Animated.add(
+        Animated.add(
+            x,
+            x.interpolate({
+                inputRange: [0, 0.00001 + index * itemWidth],
+                outputRange: [0, -index * itemWidth],
+                extrapolateRight: "clamp",
+            })
+        ),
+        position.interpolate({
+            inputRange: [isBottom, isAppearing],
+            outputRange: [0, -itemWidth / 4],
+            extrapolate: "clamp",
+        })
+    );
+    const translateY = Animated.add(
+        Animated.add(
+            y,
+            y.interpolate({
+                inputRange: [0, 0.00001 + index * itemHeight],
+                outputRange: [0, -index * itemHeight],
+                extrapolateRight: "clamp",
+            })
+        ),
+        position.interpolate({
+            inputRange: [isBottom, isAppearing],
+            outputRange: [0, -itemHeight / 4],
+            extrapolate: "clamp",
+        })
+    );
+    const scale = position.interpolate({
+        inputRange: [isDisappearing, isTop, isBottom, isAppearing],
+        outputRange: [0.5, 1, 1, 0.5],
+        extrapolate: "clamp",
+    });
+    const opacity = position.interpolate({
+        inputRange: [isDisappearing, isTop, isBottom, isAppearing],
+        outputRange: [0.0, 1, 1, 0.0],
+    });
+
+    return [translateX, translateY, scale, opacity]
+};
