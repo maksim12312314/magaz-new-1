3ee30a799990db04b6daee5fe42883c3094a3030
pBinarySoul <pBinarySoul@gmail.com>
2020-10-24 09:18:09 -0400

    Decreased delay

--- components/OurIconButton/index.js
+++ components/OurIconButton/index.js
@@ -3,22 +3,26 @@ import { TouchableOpacity } from "react-native";
 import styles from "./styles";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
-const LONG_PRESS_DELAY = 200;
+const LONG_PRESS_DELAY = 150;
 
 const OurIconButton = (props) => {
     const { onPress, doLongPress, longPressDelay, icon, size, children, style } = props;
 
     let timer = null;
+    const clearTimer = () => {
+        if ( timer )
+            clearInterval(timer);
+    };
     const onLongPress = (e) => {
         if ( doLongPress ) {
+            clearTimer();
             timer = setInterval(() => {
                 onPress(e);
             }, longPressDelay || LONG_PRESS_DELAY );
         }
     };
     const onPressOut = (e) => {
-        if ( timer )
-            clearInterval(timer);
+        clearTimer();
     };
 
     return (
