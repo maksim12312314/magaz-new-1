99d43abe0b1cf6daa6f24646f0e56eeab50909d1
pbinarysoul <pBinarySoul@gmail.com>
2020-12-04 13:24:22 +0300

    User Check fix

--- components/pages/UserCheck/index.js
+++ components/pages/UserCheck/index.js
@@ -4,22 +4,30 @@ import { STORE_ADDRESS } from "~/config";
 import { getUserRegisterQuery, getUserLoginQuery } from "~/queries";
 import { stateContext, dispatchContext } from "~/contexts";
 import { USER_STATUS_REGISTERED } from "~/userStatus";
+import { SetUserData } from "~/actions";
 import { HeaderTitle } from "~/components/Header";
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
 import { randomString } from "~/utils";
 import OurActivityIndicator from "~/components/OurActivityIndicator";
+import OurText from "~/components/OurText";
 import styles from "./styles";
 
 const UserCheck = (props) => {
     const state = useContext(stateContext);
     const dispatch = useContext(dispatchContext);
 
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
     const { navigation, nextPage } = props;
     const [gradStart, gradMiddle, gradEnd] = ["#B0E8E4", "#86A8E7","#7F7FD5"];
+
     const isRegistered = state.user.status === USER_STATUS_REGISTERED;
     const userUuid = uuidv4();
-    const regQuery = getUserRegisterQuery(userUuid, state.deliveryDetails.email.value, randomString(32), randomString(16));
+    const userName = randomString(32);
+    const userPass = randomString(32);
+    const regQuery = getUserRegisterQuery(userUuid, state.deliveryDetails.email.value, userName, userPass);
     console.log(regQuery);
 
     const fetchData = async () => {
@@ -32,7 +40,22 @@ const UserCheck = (props) => {
                                     getUserLoginQuery(state.user.uuid, state.user.username, state.user.password),
         });
         const data = await res.json();
-        console.log("data", data.data, data.errors);
+        console.log("HEHE DATA1", state.user);
+        if ( data?.data?.registerUser.user ) {
+            const userData = {
+                status: USER_STATUS_REGISTERED, // Состояние пользователя
+                uuid: userUuid,
+                username: userName,
+                email: state.deliveryDetails.email.value,
+                password: userPass,
+                jwtAuthToken: data.data.registerUser.user.jwtAuthToken,
+                jwtRefreshToken: data.data.registerUser.user.jwtRefreshToken,
+            }
+            console.log("HEHE DATA", userData);
+            dispatch(SetUserData(userData));
+        } else {
+            setError(data.message);
+        }
     };
     useEffect(() => {
         fetchData();
@@ -49,14 +72,19 @@ const UserCheck = (props) => {
         });
     }, [navigation]);
 
+    
     return (
         <>
             <LinearGradient
                 style={styles.background}
                 locations={[0, .8, 1]}
                 colors={[gradStart, gradMiddle, gradEnd]} />
-
-            <OurActivityIndicator />
+            {
+                loading && !error ?
+                    <OurActivityIndicator />
+                :
+                    <OurText>Something went wrong</OurText>
+            }
         </>
     );
 };
